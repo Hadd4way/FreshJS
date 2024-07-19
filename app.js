b5c9a4240a07d0fa7fedef77d24657ab5824861b
hadd4way <Wazeronid@gmail.com>
2024-07-19 17:19:50 +0400

    Упражнение 4 - Проверка прав

--- app.js
+++ app.js
@@ -2,21 +2,26 @@
 
 /*
 
-    Метод prompt получите ответ пользователя
-    на вопрос "Сколько будет 7 + или - 15?". Если ответ верен
-    выведите в консоле "Успех", если нет - "вы робот!"
-    а если он введет "я не робот", то тоже успех
+    Пользователь хочет приобрести игру в магазине
+    Он может это сделать только если:
+    - Его баланс больше 1000 (balance)
+    или число бонусов больше 100 (bonusbalance)
+    - Он не забанен (isBanned)
+    - Игра не куплена (isExist)
+    - Игра в продаже (isSelling)
+    Напишите условие для покупки и выведите в консоль
+    результат
 
 */
 
-let ans = prompt('Сколько будет 7 + или - 15?')
+'use strict';
 
-switch (true){
-    case ans == 'Я не робот':
-    case ans == '22':
-    case ans == '-8':
-        console.log('Успех')
-        break;
-    default:
-        console.log('Вы робот!');
-}
+let balance = 1000;
+let bonusBalance = 101;
+let isBanned = false;
+let isExist = false;
+let isSelling = true;
+
+const canbuy =(balance > 1000 || bonusBalance > 100) && !isBanned && !isExist && isSelling;
+
+console.log(`Могу купить игру: ${canbuy ? 'Да' : 'Нет'}`);
