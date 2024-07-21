e4dfc1da570542c6a125a79efe2ed9b2df81fd68
hadd4way <Wazeronid@gmail.com>
2024-07-21 13:03:28 +0400

    Упражнение 6 - Обновление списка задач

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 // Видео-курс
 
-// Упрощение функции
+/*
 
-function toPower(num, power){
-    const res = num**power;
-    return res;
+    Дан список задач
+    const tasks = ['Задача 1']
+    Сделать функции:
+    - Добавление задачи в конец
+    - Удаление задачи по названию
+    - Перенос задачи в начало списка по названию
+    Всегда меняем исходный массив
+
+
+*/
+
+const tasks = ['Задача 1']
+
+function addTask(task){
+    tasks.push(task);
 }
 
-const toPowerArrow = (num,power) => num**power;
-console.log()
+function deleteTask(task){
+    let index = tasks.indexOf(task);
+    if (index == -1){
+        return;
+    }
+    tasks.splice(tasks.indexOf(task),1);
+}
+
+function unshiftTask(task){
+    let index = tasks.indexOf(task);
+    if (index == -1){
+        return;
+    }
+    tasks.splice(index,1);
+    tasks.unshift(task);
+    
+
+}
+
+addTask('Задача 2')
+addTask('Задача 3')
+console.log(tasks)
+// deleteTask('Задача 1');
+// console.log(tasks)
+unshiftTask('Задача 3')
+console.log(tasks)
